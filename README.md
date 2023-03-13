456283f13be6b24c9fadc143b46609607289dd07
Ursula Parson <ursula.parson@gmail.com>
2023-03-13 10:27:53 -0400

    second draft of my Cypress skill assessment

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # cypress_assessment
+
+You can choose the environment on which you want to test in a few places. One of which would probably be the most helpful in earlier stages of development would be your local environment, which you can add to `cypress.config.js` in the root of this repository.
+
+The first step in running a Cypress test is running Cypress, which you can do by opening the command line & running `npx cypress open`. A window that looks like this will open in your default browser:
+
+[running_test1]
+
+Choose the E2E Testing option, then choose your preferred browser.
+
+[running_test2]
+
+You'll be redirected to a directory contains all of your `e2e` tests. Click `new_contact.cy.js`, and the tests in that file will run automatically (note: the tests in the aforementioned file will all fail; this is expected).
+
+[running_test3]
+
+If you want to run an individual test in `new_contact.cy.js`, open the file in a text editor.
+
+[running_test4]
+
+Add `x` to the `it` next to the test you'd like to leave out, or next to the entire `describe` if you'd like to leave out the entire suite. When the individual test runs, you'll see the test name without it running.
+
+[running_test6]
+
+[running_test7]
+
+And you'll see the describe name without it running when you add an `x` to `describe`.
+
+[running_testA]
+
+[running_testB]
--- cypress/e2e/new_contact.cy.js
+++ cypress/e2e/new_contact.cy.js
@@ -1,16 +1,17 @@
 import basic_information from '../fixtures/basic_information.json';
 import custom_information from '../fixtures/custom_information.json';
 
-const cool_website = "https://cool_website.cool";
+const cool_website = "https://coolwebsite.cool";
+
+let customer_number = custom_information[custom_information.customer_number];
 
 describe('Creating a new contact', () => {
-    it('contact type verification', () => {
+    it('should have Transferee as the selected contact type', () => {
         cy.visit(cool_website);
-        cy.get('select').select('contact_type').should('have.text', custom_information.contact_type);
-        // Assigned To and Agent Owner dropdowns should be ignored, as per the assessment requirements.
-      })
+        cy.get('select#contact_type option:selected').should('have.text', custom_information.contact_type);
+    })
 
-    it('new user', () => {
+    it('should create a new contact with specific information', () => {
       cy.visit(cool_website);
       cy.get('#customer_number').type(custom_information.customer_number);
       cy.get('#first_name').type(basic_information.first_name);
@@ -21,5 +22,21 @@ describe('Creating a new contact', () => {
       cy.get('#department').type(basic_information.department);
       cy.get('#primary_email').type(basic_information.primary_email);
       cy.get('#secondary_email').type(basic_information.secondary_email);
+      cy.get('button[class="save"]').click();
     })
-  })
+
+    it('should have successfully created the new contact', () => {
+        cy.request({
+         url: cool_website + '/users',
+         qs: {
+          customer_number: custom_information.customer_number,
+         },
+        })
+        .its('body')
+        .should('be.an', 'array')
+        .and('have.length', 1)
+        .its(0)
+        .should('have.property', 'primary_email')                                  // the only other property that I expect to be completely unique
+        .and('contain', custom_information[customer_number].primary_email)
+  });
+});
